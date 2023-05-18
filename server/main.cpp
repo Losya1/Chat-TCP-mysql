0d590e4f245eeda595ae21a65364cbdc0ba12205
Losya1 <121752107+Losya1@users.noreply.github.com>
2023-05-18 21:45:20 +0300

    Add files via upload

--- server/main.cpp
+++ server/main.cpp
@@ -2,18 +2,22 @@
 #include <WinSock2.h>
 #include <WS2tcpip.h>
 #include <vector>
+#include "mysql.h"
 
 #pragma comment(lib, "Ws2_32.lib")
 
 using namespace std;
 
 int main() {
-	const char IP_SERV[] = "127.0.0.1";			// Enter local Server IP address
-	const int PORT_NUM = 7777;				// Enter Open working server port
-	const short BUFF_SIZE = 1024;			// Maximum size of buffer for exchange info between server and client
-	int erStat;								// Keeps socket errors status
+	const char IP_SERV[] = "127.0.0.1";			
+	const int PORT_NUM = 7777;			
+	const short BUFF_SIZE = 1024;		
+	int erStat;								
+	MYSQL_RES* res;
+	MYSQL_ROW row;
+	
+	//SOCKET
 
-	//IP in string format to numeric format for socket functions. Data is in "ip_to_num"
 	in_addr ip_to_num;
 	erStat = inet_pton(AF_INET, IP_SERV, &ip_to_num);
 
@@ -22,7 +26,6 @@ int main() {
 		return 1;
 	}
 
-	// WinSock initialization
 	WSADATA wsData;
 
 	erStat = WSAStartup(MAKEWORD(2, 2), &wsData);
@@ -36,7 +39,6 @@ int main() {
 		cout << "WinSock initialization is OK" << endl;
 	}
 
-	// Server socket initialization
 	SOCKET ServSock = socket(AF_INET, SOCK_STREAM, 0);
 
 	if (ServSock == INVALID_SOCKET) {
@@ -49,9 +51,8 @@ int main() {
 		cout << "Server socket initialization is OK" << endl;
 	}
 
-	// Server socket binding
 	sockaddr_in servInfo;
-	ZeroMemory(&servInfo, sizeof(servInfo));	// Initializing servInfo structure
+	ZeroMemory(&servInfo, sizeof(servInfo));
 
 	servInfo.sin_family = AF_INET;
 	servInfo.sin_addr = ip_to_num;
@@ -69,7 +70,6 @@ int main() {
 		cout << "Binding socket to Server info is OK" << endl;
 	}
 
-	//Starting to listen to any Clients
 	erStat = listen(ServSock, SOMAXCONN);
 
 	if (erStat != 0) {
@@ -82,9 +82,8 @@ int main() {
 		cout << "Listening..." << endl;
 	}
 
-	//Client socket creation and acception in case of connection
 	sockaddr_in clientInfo;
-	ZeroMemory(&clientInfo, sizeof(clientInfo));	// Initializing clientInfo structure
+	ZeroMemory(&clientInfo, sizeof(clientInfo));	
 
 	int clientInfo_size = sizeof(clientInfo);
 
@@ -101,24 +100,83 @@ int main() {
 		cout << "Connection to a client established successfully" << endl;
 		char clientIP[22];
 
-		inet_ntop(AF_INET, &clientInfo.sin_addr, clientIP, INET_ADDRSTRLEN);	// Convert connected client's IP to standard string format
+		inet_ntop(AF_INET, &clientInfo.sin_addr, clientIP, INET_ADDRSTRLEN);	
 
 		cout << "Client connected with IP address " << clientIP << endl;
 	}
 
-	//Exchange text data between Server and Client. Disconnection if a client send "xxx"
 
-	char Client_message[BUFF_SIZE];
+	//MYSQL
+
+	MYSQL mysql; 
+	mysql_init(&mysql);
+
+	if (&mysql == NULL) {
+		// Если дескриптор не получен — выводим сообщение об ошибке
+		cout << "Error: can't create MySQL-descriptor" << endl;
+	}
+
+	// Подключаемся к серверу
+	if (!mysql_real_connect(&mysql, "localhost", "root", "feg123", "testdb", 0, NULL, 0)) {
+		// Если нет возможности установить соединение с БД выводим сообщение об ошибке
+		cout << "Error: can't connect to database " << mysql_error(&mysql) << endl;
+	}
+	else {
+		// Если соединение успешно установлено выводим фразу — "Success!"
+		cout << "Success!" << endl;
+	}
+
+	mysql_set_character_set(&mysql, "utf8");
+	//Смотрим изменилась ли кодировка на нужную, по умолчанию идёт latin1
+	cout << "connection characterset: " << mysql_character_set_name(&mysql) << endl;
+
+	vector <char> Client_message(BUFF_SIZE);
 	short cmes = 0, smes = 0;
-
+	string x, y;
 	while (true) {
 		cout << "Waiting for the message" << endl;
 
-		cmes = recv(ClientConn, Client_message, BUFF_SIZE, 0);	// Receiving packet from client. Program is waiting (system pause) until receive
+		cmes = recv(ClientConn, Client_message.data(), BUFF_SIZE, 0);
 
-		cout << "Client's message: " << Client_message << endl;
+		if (Client_message.at(0) == '1') {
+			Client_message.erase(Client_message.begin());
+			x = Client_message.data();
+			mysql_query(&mysql, "INSERT INTO user(id, name) values(default, x)");
+		}
 
-		smes = send(ClientConn, Client_message, BUFF_SIZE, 0);
+		if (Client_message.at(0) == '2') {
+			Client_message.erase(Client_message.begin());
+			y = Client_message.data();
+			mysql_query(&mysql, "INSERT INTO user(id, name) values(default, y)");
+		}
+
+		if (Client_message.at(0) == '3') {
+			Client_message.erase(Client_message.begin());
+			mysql_query(&mysql, "SELECT username FROM user");
+			if (res = mysql_store_result(&mysql)) {
+				while (row = mysql_fetch_row(res)) {
+					for (int i = 0; i < mysql_num_fields(res); i++) {
+						cout << row[i] << "  ";
+					}
+					cout << endl;
+				}
+			}
+		}
+
+		if (Client_message.at(0) == '4') {
+			Client_message.erase(Client_message.begin());
+			mysql_query(&mysql, "SELECT password FROM user");
+			if (res = mysql_store_result(&mysql)) {
+				while (row = mysql_fetch_row(res)) {
+					for (int i = 0; i < mysql_num_fields(res); i++) {
+						cout << row[i] << "  ";
+					}
+					cout << endl;
+				}
+			}
+		}
+
+		smes = send(ClientConn, Client_message.data(), BUFF_SIZE, 0);
 
 		if (smes == SOCKET_ERROR) {
 			cout << "Can't send message to Client. Error # " << WSAGetLastError() << endl;
@@ -129,7 +187,8 @@ int main() {
 		}
 	}
 
-	cmes = 0, smes = 0;
+	mysql_close(&mysql);
+
 	closesocket(ServSock);
 	closesocket(ClientConn);
 	WSACleanup();
